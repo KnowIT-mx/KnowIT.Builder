9ba0fbb583b8e517e0d36f5d193b325cdcd31808
José Ramón [KnowIT] <jraguilar@knowit.mx>
2026-08-06 21:18:02 -0600

    Add `Test-KnowITModule` function
    Run tests included in module using Pester

--- src/private/Common.ps1
+++ src/private/Common.ps1
@@ -1,1 +1,11 @@
 ﻿
+function AssertPesterModule {
+    $pester = Get-Module -Name Pester -ListAvailable -Verbose:$false
+    | Sort-Object -Property Version -Descending
+    | Select-Object -First 1
+    
+    if(!$pester -or $pester.Version.Major -lt 5) {
+        throw "Pester module v5 or later is not installed. Please install it from the PowerShell Gallery. Run the following command:`n
+        'Install-PSResource -Name Pester -Version '[5.0.0,)' -Repository PSGallery'"
+    }
+}
---
+++ src/public/Test-KnowITModule.ps1
@@ -0,0 +1,96 @@
+﻿function Test-KnowITModule {
+
+    [CmdletBinding()]
+    [Alias('test')]
+    param(
+        [Alias('Configuration')]
+        [hashtable]$PesterConfiguration = @{},
+
+        [Alias('ModulePath')]
+        [string]$ImportModule, 
+        
+        [switch]$NoThrow,
+
+        [Parameter(ParameterSetName = 'Job')]
+        [switch]$AsJob, 
+
+        [Parameter(ParameterSetName = 'Job')]
+        [ValidateSet('5.1')]
+        [version]$JobVersion,
+
+        [Parameter(ParameterSetName = 'Job')]
+        [scriptblock]$InitializationScript
+    )
+
+    try {
+        Update-CallerPreference $PSCmdlet
+
+        AssertPesterModule
+        $PesterConfiguration.Run ??= @{}
+        $PesterConfiguration.Run.Path ??= $PWD.Path
+        if($ImportModule) {
+            $ImportModule = Convert-Path $ImportModule
+        }
+
+        $script = {
+            param($PesterConfiguration, $ImportModule, $DebugPreference)
+            
+            $VerbosePreference = 'SilentlyContinue'
+            $DebugPreference = $DebugPreference
+            Write-Debug ($PSVersionTable | Out-String)
+            
+            Import-Module Pester
+           # Pester v5 does not support ArrayLists in the configuration Hashtable
+            foreach($section in $PesterConfiguration.Values) {
+                foreach($key in @($section.Keys)) {
+                    if($section[$key] -is [Collections.ArrayList]) {
+                        $section[$key] = $section[$key].ToArray()
+                    }
+                }
+            }   
+            Write-Debug "Pester configuration: $($PesterConfiguration | Out-String)"
+            $pesterConfig = New-PesterConfiguration -Hashtable $PesterConfiguration
+            $pesterConfig.Run.PassThru = $true
+
+            if($ImportModule) {
+                Import-Module $ImportModule -Force
+            }
+            Invoke-Pester -Configuration $pesterConfig
+        }
+
+        $testOutput = if($AsJob) {
+            $params = @{
+                ScriptBlock = $script
+                ArgumentList = @($PesterConfiguration, $ImportModule, $DebugPreference)
+            }
+            if($InitializationScript) {
+                $params.InitializationScript = $InitializationScript
+            }
+            if($JobVersion) {
+                Write-Build "[TEST] Running tests in a PowerShell $JobVersion job..." Magenta
+                $params.PSVersion = $JobVersion
+            }
+            else {
+                Write-Build '[TEST] Running tests in a PowerShell job...' Magenta
+            }
+            Start-Job @params | Receive-Job -Wait -AutoRemoveJob
+        }
+        else {
+            Write-Build '[TEST] Running tests in the current PowerShell session...' Magenta
+            Invoke-Command $script -ArgumentList $PesterConfiguration, $ImportModule, $DebugPreference
+        }
+
+        if(!$testOutput) {
+            throw 'There was a problem running the tests.'
+        }
+        if($NoThrow) {
+            return $testOutput
+        }   
+        if($testOutput.FailedCount -gt 0) {
+            throw "$($testOutput.FailedCount) test(s) failed!"
+        }
+    }
+    catch {
+        $PSCmdlet.WriteError($_)
+    }
+}
